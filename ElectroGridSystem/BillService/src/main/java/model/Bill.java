7795ea421ca2f326c912e3800226c102361701a7
Yomal Bandara <yomalb07@gmail.com>
2022-04-18 22:04:26 +0530

    implemented the db connect method

---
+++ ElectroGridSystem/BillService/src/main/java/model/Bill.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Bill {
+	
+	//A common method to connect to the DB
+	private Connection connect() 
+	 { 
+		Connection con = null; 
+		 try
+		 { 
+			 Class.forName("com.mysql.jdbc.Driver"); 
+		 
+			 //Provide the correct details: DBServer/DBName, username, password 
+			 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogriddb", "root", "");
+			 
+		 } 
+		 catch (Exception e) 
+		 {
+			 e.printStackTrace();
+		 } 
+		 return con; 
+	 } 
+
+}
