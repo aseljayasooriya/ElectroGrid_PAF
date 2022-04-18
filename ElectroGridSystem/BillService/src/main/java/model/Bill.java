0e53b337efc5ca06525e685de5688577724ec63d
Yomal Bandara <yomalb07@gmail.com>
2022-04-18 22:40:52 +0530

    implemented insert method in the model class

--- ElectroGridSystem/BillService/src/main/java/model/Bill.java
+++ ElectroGridSystem/BillService/src/main/java/model/Bill.java
@@ -2,6 +2,7 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class Bill {
 	
@@ -22,6 +23,52 @@ public class Bill {
 			 e.printStackTrace();
 		 } 
 		 return con; 
+	 }
+	
+	//Insert method 
+	public String insertBill(String billCode, String accountNo, String billMonth, String units,String meterReader_name) 
+	 { 
+		 String output = "";
+		 
+		 //bill calculation
+		 //Conversion of the String variable into double
+		 double unit = Double.parseDouble(units);
+		 double unitPrice = 50.00;
+		 
+		 //calculation
+		 double amount = unit*unitPrice;
+		 try
+		 { 
+			 Connection con = connect(); 
+			 if (con == null) 
+			 {
+				 return "Error while connecting to the database when inserting the bill."; 
+			 } 
+			 // create a prepared statement
+			 String query = " insert into bill (`billID`,`billCode`,`accountNo`,`billMonth`,`units`,`billAmount`,`meterReader_name`)"
+			 + " values (?, ?, ?, ?, ?, ?, ?)"; 
+			 PreparedStatement preparedStmt = con.prepareStatement(query); 
+			 
+			 // binding values
+			 preparedStmt.setInt(1, 0); 
+			 preparedStmt.setString(2, billCode); 
+			 preparedStmt.setString(3, accountNo);
+			 preparedStmt.setString(4, billMonth);
+			 preparedStmt.setString(5, units);
+			 preparedStmt.setDouble(6, amount);
+			 preparedStmt.setString(7, meterReader_name); 
+			 
+			 // execute the statement
+			 preparedStmt.execute(); 
+			 con.close(); 
+			 output = "Inserted successfully"; 
+		 } 
+		 catch (Exception e) 
+		 { 
+			 output = "Error while inserting the bill."; 
+			 System.err.println(e.getMessage()); 
+		 } 
+		 return output; 
 	 } 
 
 }
