fda6f6a60d0c4090ef4695db7be205f48d06bdba
aseljayasooriya <asel.jaya@gmail.com>
2022-04-19 13:10:15 +0530

    change the runtime env of the project and restructured

--- ElectroGridSystem/BreakdownInformationService/src/main/java/com/BreakDownService.java
+++ ElectroGridSystem/BreakdownInformationService/src/main/java/com/BreakDownService.java
@@ -1,5 +1,29 @@
 package com;
 
-public class BreakDownService {
+import model.BreakDown;
 
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document; 
+
+@Path("/Breakdowns")
+public class BreakDownService {
+	
+	BreakDown breakdownObj = new BreakDown();
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readBreakdowns()
+	{
+		return "Hello";
+	} 
 }
--- ElectroGridSystem/BreakdownInformationService/src/main/java/model/BreakDown.java
+++ ElectroGridSystem/BreakdownInformationService/src/main/java/model/BreakDown.java
@@ -6,24 +6,62 @@ public class BreakDown {
 	
 	//A common method to connect to the DB
 	private Connection connect()
-	 {
-		 Connection con = null;
-		 
-		 try
-		 {
-			 Class.forName("com.mysql.jdbc.Driver");
+	{
+		Connection con = null;
+			 
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			//Provide the database details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogriddb", "root", "");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+			
+		return con;
+	}	
+	
+	public String insertItem(String bsector, String bdate, String sTime, String eTime, String btype) {
 		
-			 //Provide the database details: DBServer/DBName, username, password
-			 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogriddb", "root", "");
-		 }
-		 catch (Exception e)
-		 {
-			 e.printStackTrace();
-		 }
-		 
-		 return con;
-	 }
-	
-	
+		String output = "";
+		
+		try {
+			Connection con = connect();
+			
+			if(con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			
+			//creating the prepared statement
+			String query = " insert into breakdowninformation (`breakdownID`,`breakdownSector`,`breakdownDate`,`startTime`,`endTime`,`breakdownType`)"
+							+ " values (?, ?, ?, ?, ?, ?)";
+			
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			//binding the values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, bsector);
+			preparedStmt.setDate(3, Date.valueOf(bdate));
+			preparedStmt.setTime(4, Time.valueOf(sTime));
+			preparedStmt.setTime(5, Time.valueOf(eTime));
+			preparedStmt.setString(6, btype);
+			
+			//execute the statement
+			preparedStmt.execute();
+			con.close();
+			
+			output = "Inserted successfully";
+			
+		}
+		catch (Exception e) {
+			output = "Error while inserting the breakdown info.";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
 
 }
