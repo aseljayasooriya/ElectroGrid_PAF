c90cc8ecf84c855476862d01838740d209efc566
Yomal Bandara <yomalb07@gmail.com>
2022-04-19 17:07:21 +0530

    Implemented the insertBill method with the calculation

--- ElectroGridSystem/BillService/src/main/java/model/Bill.java
+++ ElectroGridSystem/BillService/src/main/java/model/Bill.java
@@ -17,4 +17,50 @@ public class Bill {
 		return con;
 	}
 
+	//Insert method 
+	public String insertBill(String billCode, String accountNo, String billMonth, String units,String meterReader_name) 
+	 { 
+		 String output = "";
+		 
+		 //bill calculation
+		 //Conversion of the String variable into double
+		 double unit = Double.parseDouble(units);
+		 double unitPrice = 50.00;
+		 //calculation
+		 double tot = unit*unitPrice;
+		 String amount = Double.toString(tot);
+		 try
+		 { 
+			 Connection con = connect(); 
+			 if (con == null) 
+			 {
+				 return "Error while connecting to the database when inserting the bill."; 
+			 } 
+			 // create a prepared statement
+			 String query = " insert into bill (`billID`,`billCode`,`accountNo`,`billMonth`,`units`,`billAmount`,`meterReader_name`)"
+			 + " values (?, ?, ?, ?, ?, ?, ?)"; 
+			 PreparedStatement preparedStmt = con.prepareStatement(query); 
+			 
+			 // binding values
+			 preparedStmt.setInt(1, 0); 
+			 preparedStmt.setString(2, billCode); 
+			 preparedStmt.setString(3, accountNo);
+			 preparedStmt.setString(4, billMonth);
+			 preparedStmt.setString(5, units);
+			 preparedStmt.setString(6, amount);
+			 preparedStmt.setString(7, meterReader_name); 
+			 
+			 // execute the statement
+			 preparedStmt.execute(); 
+			 con.close(); 
+			 output = "Inserted successfully"; 
+		 } 
+		 catch (Exception e) 
+		 { 
+			 output = "Error while inserting the bill."; 
+			 System.err.println(e.getMessage()); 
+		 } 
+		 return output; 
+	 }
+
 }
