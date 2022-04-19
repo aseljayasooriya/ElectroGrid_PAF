135be1e9dd3eea60d272556bb91082f927beea85
Yomal Bandara <yomalb07@gmail.com>
2022-04-19 17:11:37 +0530

    Imported the packages to the API class

--- ElectroGridSystem/BillService/src/main/java/com/BillService.java
+++ ElectroGridSystem/BillService/src/main/java/com/BillService.java
@@ -1,5 +1,15 @@
 package com;
 
+import model.Bill;
+
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType;
+
+
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document;
+
 public class BillService {
 
 }
