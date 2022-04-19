c6973d69e5235cd85296d2de0a803e6275058e5e
Yomal Bandara <yomalb07@gmail.com>
2022-04-19 17:14:59 +0530

    Implemented the API method to insert

--- ElectroGridSystem/BillService/src/main/java/com/BillService.java
+++ ElectroGridSystem/BillService/src/main/java/com/BillService.java
@@ -10,6 +10,25 @@ import org.jsoup.*;
 import org.jsoup.parser.*; 
 import org.jsoup.nodes.Document;
 
+@Path("/Bills")
 public class BillService {
+	
+	//Object creation
+	Bill billObj = new Bill();
+	
+	@POST
+	@Path("/")
+	//to specify the input type as form data
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	//produce a status message as an output
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertBill(@FormParam("billCode") String billCode,@FormParam("accountNo") String accountNo,
+            				 @FormParam("billMonth") String billMonth, @FormParam("units") String units, 
+            				 @FormParam("meterReader_name") String meterReader_name) 
+	{ 
+		String output = billObj.insertBill(billCode, accountNo, billMonth, units, meterReader_name);
+		return output;
+	
+	}
 
 }
