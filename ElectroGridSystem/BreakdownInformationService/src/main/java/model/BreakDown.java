afb0551478c1ec1cbd0784aaf53debe462cd60e1
aseljayasooriya <asel.jaya@gmail.com>
2022-04-20 14:01:15 +0530

    Implemented the class to obtain the sector breakdowns

--- ElectroGridSystem/BreakdownInformationService/src/main/java/model/BreakDown.java
+++ ElectroGridSystem/BreakdownInformationService/src/main/java/model/BreakDown.java
@@ -196,5 +196,72 @@ public class BreakDown {
 		
 		return output;
 	}
+	
+public String readSectorBreakdowns(String bID) {
+		
+		String output = "";
+		
+		try {
+			
+			Connection con = connect();
+			
+			if(con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			
+			
+			output = "<table border='1'><tr><th>Breakdown Sector</th><th>Breakdown Date</th>" +
+					 "<th>Breakdown Start Time</th>" +
+					 "<th>Breakdown End Time</th>" +
+					 "<th>Breakdown Type</th>" +
+					 "<th>Update</th><th>Remove</th></tr>";
+			
+			String query = "SELECT * FROM breakdowninformation WHERE breakdownID=?";
+			
+			
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			//binding values
+			preparedStmt.setInt(1, Integer.parseInt(bID));
+			
+			
+			ResultSet rs = preparedStmt.executeQuery();
+			
+			
+			while(rs.next()) {
+				String breakdownID = Integer.toString(rs.getInt("breakdownID"));
+				String breakdownSector = rs.getString("breakdownSector");
+				String breakdownDate = rs.getString("breakdownDate");
+				String startTime = rs.getString("startTime");
+				String endTime = rs.getString("endTime");
+				String breakdownType = rs.getString("breakdownType");
+				
+				//add into html table
+				output += "<tr><td>" + breakdownSector + "</td>";
+				output += "<td>" + breakdownDate + "</td>";
+				output += "<td>" + startTime + "</td>";
+				output += "<td>" + endTime + "</td>";
+				output += "<td>" + breakdownType + "</td>";
+				
+				//buttons
+				output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
+						 + "<td><form method='post' action='breakdowns.jsp'>"
+						 + "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
+						 + "<input name='breakdownID' type='hidden' value='" + breakdownID
+						 + "'>" + "</form></td></tr>"; 
+			}
+			
+			con.close();
+			
+			//complete the html table
+			output += "</table>"; 
+		}
+		catch (Exception e) {
+			output = "Error while reading the breakdown info.";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
 
 }
