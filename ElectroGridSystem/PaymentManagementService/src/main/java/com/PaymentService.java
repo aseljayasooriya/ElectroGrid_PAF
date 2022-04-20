b89101425ec471da9deb2b7b422288fcc571648b
aseljayasooriya <asel.jaya@gmail.com>
2022-04-20 22:20:09 +0530

    Create crud function implemented in payment management service

--- ElectroGridSystem/PaymentManagementService/src/main/java/com/PaymentService.java
+++ ElectroGridSystem/PaymentManagementService/src/main/java/com/PaymentService.java
@@ -1,5 +1,36 @@
 package com;
 
+import model.Payment;
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/Payments")
 public class PaymentService {
+	
+	Payment paymentObj = new Payment();
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertBreakdown(@FormParam("accountNo") String accountNo,
+			 						@FormParam("paymentAmount") String paymentAmount,
+			 						@FormParam("paymentMethod") String paymentMethod,
+			 						@FormParam("cardNo") String cardNo,
+			 						@FormParam("email") String email) {
+		
+		String output = paymentObj.insertPayment(accountNo, paymentAmount, paymentMethod, cardNo, email);
+		return output;
+	}
 
 }
