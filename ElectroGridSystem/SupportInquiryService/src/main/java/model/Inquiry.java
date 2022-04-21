c3ab398529e1ceb55e7cb90e01ff9a12f3ffca51
Wishwa98 <wishwa5020@gmail.com>
2022-04-20 23:23:01 -0700

    Insert method Added

--- ElectroGridSystem/SupportInquiryService/src/main/java/model/Inquiry.java
+++ ElectroGridSystem/SupportInquiryService/src/main/java/model/Inquiry.java
@@ -20,6 +20,45 @@ public class Inquiry {
 	 return con;
 	 } 
 	
+	//Create Insert method
+	
+	public String insertInquiry(String title,String description, int contact) {
+		String output ="";
+		
+		try 
+		{
+			Connection con = connect();
+			
+			if(con ==null)
+			{
+				return "Error while connecting to the database for inserting.";
+			}
+			
+			//creating prepared statement
+			String query = "insert into inquiry ('inquiryID','inquiryTitle','inquiryDesc','contactNum')"+"values(?,?,?,?)";
+			
+			PreparedStatement preparedStmt =con.prepareStatement(query);
+			
+			//binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, title);
+			preparedStmt.setString(3, description);
+			preparedStmt.setInt(4,contact);
+			
+			//execute the statements
+			preparedStmt.execute();
+			con.close();
+			
+			output = "inserted successfully";	
+			}
+		catch (Exception e) {
+			output="Error while inserting the inquiry";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+	
 	
 	
 	
