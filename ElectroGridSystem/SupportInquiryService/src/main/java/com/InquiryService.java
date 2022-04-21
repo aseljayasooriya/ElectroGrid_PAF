692dc5476d19462ba1a42c0968d81cbf0a12cf23
Wishwa98 <wishwa5020@gmail.com>
2022-04-21 00:13:28 -0700

    Import packages to RESTful API class

--- ElectroGridSystem/SupportInquiryService/src/main/java/com/InquiryService.java
+++ ElectroGridSystem/SupportInquiryService/src/main/java/com/InquiryService.java
@@ -1,5 +1,20 @@
 package com;
 
+import model.Inquiry;
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document; 
+
+@Path("/Inquiry")
 public class InquiryService {
 
 }
