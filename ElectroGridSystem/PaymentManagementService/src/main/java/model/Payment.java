5ed7a325f4d1386c05f35a3000d94628e8c1d0a8
Yomal Bandara <yomalb07@gmail.com>
2022-04-21 13:29:58 +0530

    Implemented the updatePayment method in model class

--- ElectroGridSystem/PaymentManagementService/src/main/java/model/Payment.java
+++ ElectroGridSystem/PaymentManagementService/src/main/java/model/Payment.java
@@ -130,6 +130,45 @@ public class Payment {
 		
 			return output;
 				}
+		
+		//Update method
+		public String updatePayment(String paymentID, String accNo, String pAmount,
+					    String pMethod, String cardNo, String pEmail)
+		{
+			String output = "";
+			
+			 try
+			 { 
+			 Connection con = connect(); 
+			 if (con == null) 
+			 {
+				 return "Error while connecting to the database for updating.";
+			 } 
+			 
+			 // create a prepared statement
+			 String query = "UPDATE payment SET accountNo=?,paymentAmount=?,paymentMethod=?,cardNo=?, email = ? WHERE paymentID=?"; 
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+			 
+			 // binding values
+			 preparedStmt.setString(1, accNo);
+			 preparedStmt.setDouble(2, Double.parseDouble(pAmount));
+		         preparedStmt.setString(3, pMethod);
+			 preparedStmt.setString(4, cardNo);
+			 preparedStmt.setString(5, pEmail);
+			 preparedStmt.setInt(6, Integer.parseInt(paymentID));
+			 
+			 // execute the statement
+			 preparedStmt.execute(); 
+			 con.close(); 
+			 output = "Updated successfully"; 
+			 } 
+			 catch (Exception e) 
+			 { 
+			 output = "Error while updating the payment."; 
+			 System.err.println(e.getMessage()); 
+			 } 
+			 return output;
+		}
 				
 		}
 
