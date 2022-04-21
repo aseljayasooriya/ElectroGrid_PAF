266df5bb53a8d331416da571cbcd55191f228392
Wishwa98 <wishwa5020@gmail.com>
2022-04-20 21:56:23 -0700

    DB connect method created

--- ElectroGridSystem/SupportInquiryService/src/main/java/model/Inquiry.java
+++ ElectroGridSystem/SupportInquiryService/src/main/java/model/Inquiry.java
@@ -1,5 +1,27 @@
 package model;
 
-public class Inquiry {
+import java.sql.*;
 
+public class Inquiry {
+	
+	//A common method to connect to the DataBase
+	private Connection connect()
+	 {
+	 Connection con = null;
+	 try
+	 {
+	 Class.forName("com.mysql.jdbc.Driver");
+
+	 //Details of the database
+	 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogrid", "root", "");
+	 }
+	 catch (Exception e)
+	 {e.printStackTrace();}
+	 return con;
+	 } 
+	
+	
+	
+	
+	
 }
